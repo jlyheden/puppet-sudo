38ee3d8a89e4bf385613b2f3d97de1b18c3fa57d
jlyheden <johan@lyheden.com>
2013-03-07 12:36:25 +0100

    syntax check for conf entries

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -25,6 +25,14 @@
 #     you know, what you're doing.
 #     Default: auto-set, platform specific
 #
+#   [*syntax_check*]
+#     If entry should be syntax checked before added.
+#     Uses visudo -c and removes the file in case
+#     syntax check fails. In case of failures you
+#     will see this resource being reapplied on
+#     every puppet run
+#     Default: platform specific
+#
 # Actions:
 #   Installs sudo configuration snippets
 #
@@ -42,7 +50,8 @@ define sudo::conf (
   $priority         = 10,
   $content          = undef,
   $source           = undef,
-  $sudo_config_dir  = 'UNDEF'
+  $sudo_config_dir  = 'UNDEF',
+  $syntax_check     = 'UNDEF'
 ) {
 
   include sudo
@@ -54,7 +63,27 @@ define sudo::conf (
     default => $sudo_config_dir
   }
 
-  Class['sudo'] -> Sudo::Conf[$name]
+  $syntax_check_real = $syntax_check ? {
+    'UNDEF' => $sudo::params::syntax_check,
+    default => $syntax_check
+  }
+
+  $dname = "${priority}_${name}"
+
+  case $syntax_check_real {
+    true: {
+      exec { "sudo-syntax-check-${dname}":
+        command     => "visudo -c -f ${sudo_config_dir_real}/${dname} || (rm -f ${sudo_config_dir_real}/${dname} && exit 1)",
+        refreshonly => true,
+        path        => [ '/bin', '/usr/bin', '/sbin', '/usr/sbin' ]
+      }
+      File[$dname] { notify => Exec["sudo-syntax-check-${dname}"] }
+    }
+    false: {}
+    default: {
+      fail("Unsupported value ${syntax_check_real} for parameter syntax_check")
+    }
+  }
 
   if $content != undef {
     $content_real = "${content}\n"
@@ -62,7 +91,7 @@ define sudo::conf (
     $content_real = undef
   }
 
-  file { "${priority}_${name}":
+  file { $dname:
     ensure  => $ensure,
     path    => "${sudo_config_dir_real}${priority}_${name}",
     owner   => 'root',
